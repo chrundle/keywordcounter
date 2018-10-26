ff5696870b4323fa153e8dbed1d4595bb1b4a8bd
chrundle <jdiffen1@gmail.com>
2018-10-26 13:27:59 -0400

    updates to main program used to return max queries of search engine at an instance; minor update to sample problem file to correct formatting

--- Source/keywordcounter.cpp
+++ Source/keywordcounter.cpp
@@ -34,9 +34,12 @@ void PrintKeysAndQueries(FibonacciHeap fheap) {
 /* Program for determining the n most popular keywords used in search engine */
 int main(int argc, char *argv[]) {
     /* Initialize variables */
-    int exit_status ;
+    int exit_status, space_num ;
+    long int frequency, query ;
     ifstream qfile ;
-    string line ;
+    ofstream outfile ;
+    string line, keyword ;
+    Node *max = new Node() ;
 
     /* Check if number of input arguments is correct */
     if (argc != 2) {
@@ -67,16 +70,109 @@ int main(int argc, char *argv[]) {
         /* Exit program */
         return exit_status ;
     }
+    #ifdef DEBUG_MAIN
+    cout << "main::DEBUG: Input file successfully opened." << endl ;
+    #endif
+
+    /* Open output file */
+    outfile.open ("output_file.txt") ;
+
+    /* Check if file was unable to be opened */
+    if (!outfile.is_open()) {/* File unable to be opened succefully */
+        /* Set exit status to 2 */
+        exit_status = 2 ;
+        /* Exit program */
+        return exit_status ;
+    }
+    #ifdef DEBUG_MAIN
+    cout << "main::DEBUG: Output file successfully opened." << endl ;
+    #endif
+
+    /* Initialize FibonacciHeap */
+#if 0
+    FibonacciHeap fheap("facebook", 5) ;
+#endif
+
+    /* Get first line from qfile */
+    getline (qfile, line) ;
 
     /* Pass through input file extracting current line at each pass */
-    while (getline (qfile, line)) {
+    while (line.compare("stop")) {/* Compore current line to stop command */
         #ifdef DEBUG_MAIN
         /* Print current line in input file */
         cout << "main::DEBUG: Current line in file: " << line << endl ;
         #endif
 
         /* Check if current line contains update or request query */
+        if (line.substr(0,1) == "$") {/* Received keyword and frequency */
+            /* Identify position of " " in line */
+            space_num = line.find(' ') ;
+
+            /* Extract keyword from line */
+            keyword = line.substr(1, space_num - 1) ;
+            /* Extract frequency from line and convert to int */
+            frequency = stoi(line.substr(space_num + 1)) ;
+
+            #ifdef DEBUG_MAIN
+            /* Print current keyword and frequency */
+            #endif
+            cout << "main::DEBUG: Current keyword, frequency: " ;
+            cout << keyword << ", " << frequency << endl ;
+
+#if 0
+            /* Update keyword and frequency in heap */
+            fheap.insert(keyword, frequency) ;
+#endif
+        }
+        else {/* Received query request */
+            /* Extract query request from line and convert to int */
+            query = stoi(line) ;
+
+            #ifdef DEBUG_MAIN
+            /* Print current query number */
+            #endif
+            cout << "main::DEBUG: Current query: " << query << endl ;
+
+#if 0
+            /* -- Remove requested number of elements from fheap and print -- */
+            while (query > 1) {
+                /* Find maximum value in heap */
+                max = fheap.find_max() ;
+                /* Print 'keyword,' to output file */
+                outfile << max->keyword << "," ;
+                /* Meld node to separate fibonacci heap */
+             
+                /* Decrease query by one */
+                query-- ;
+            }
+
+            /* Repeat operations for final query */
+            /* Find maximum value in heap */
+            max = fheap.find_max() ;
+            /* Print 'keyword' to output file and endline */
+            outfile << max->keyword << endl ;
+            /* Meld node to separate fibonacci heap */
+             
+
+            /* -- Insert removed elements back into fheap -- */
+            /* Merge separate fibonacci heap with fheap */
+#endif
+
+        }
+
+        /* Get next line and break if no next line */
+        if (!getline (qfile, line)) {/* Error occured in getline */
+            cout << "main::DEBUG: Error occured using getline." << endl ;
+            break ;
+        }
     }
+
+
+    #ifdef DEBUG_MAIN
+    /* Print current line in input file */
+    cout << "main::DEBUG: Received 'stop' command." << endl ;
+    cout << "main::DEBUG: Current line in file: " << line << endl ;
+    #endif
 
 #if 0
     /* Initialize FibonacciHeap */
@@ -140,8 +236,9 @@ int main(int argc, char *argv[]) {
     fclose(fp) ;
 #endif
 
-    /* Close file */
+    /* Close files */
     qfile.close() ;
+    outfile.close() ;
 
     /* Exit program and return exit status */
     return exit_status ;
